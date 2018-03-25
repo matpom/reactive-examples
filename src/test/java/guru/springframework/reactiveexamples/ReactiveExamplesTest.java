7a777a0ed4bf894a8208f8b5cb83bb57c93ed37e
Mateusz Pomorski <pomorski.mateusz@gmail.com>
2018-03-25 13:49:36 +0100

    ReactiveStreams playground

---
+++ src/test/java/guru/springframework/reactiveexamples/ReactiveExamplesTest.java
@@ -0,0 +1,120 @@
+package guru.springframework.reactiveexamples;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by jt on 8/24/17.
+ */
+@Slf4j
+public class ReactiveExamplesTest {
+
+  private Person michael = new Person("Michael", "Weston");
+  private Person fiona = new Person("Fiona", "Glenann");
+  private Person sam = new Person("Sam", "Axe");
+  private Person jesse = new Person("Jesse", "Porter");
+
+  @Test
+  public void monoTests() {
+    //create new person mono
+    Mono<Person> personMono = Mono.just(michael);
+
+    //get person object from mono publisher
+    Person person = personMono.block();
+
+    // output name
+    log.info(person.sayMyName());
+  }
+
+  @Test
+  public void monoTransform() {
+    //create new person mono
+    Mono<Person> personMono = Mono.just(fiona);
+
+    //type transformation
+    PersonCommand command = personMono
+        .map(PersonCommand::new).block();
+
+    log.info(command.sayMyName());
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void monoFilter() {
+    Mono<Person> personMono = Mono.just(sam);
+
+    //filter example
+    Person samAxe = personMono
+        .filter(person -> person.getFirstName().equalsIgnoreCase("foo"))
+        .block();
+
+
+    log.info(samAxe.sayMyName()); //throws NPE
+  }
+
+  @Test
+  public void fluxTest() {
+
+    Flux<Person> people = Flux.just(michael, fiona, sam, jesse);
+
+    people.subscribe(person -> log.info(person.sayMyName()));
+
+  }
+
+  @Test
+  public void fluxTestFilter() {
+
+    Flux<Person> people = Flux.just(michael, fiona, sam, jesse);
+
+    people.filter(person -> person.getFirstName().equals(fiona.getFirstName()))
+        .subscribe(person -> log.info(person.sayMyName()));
+
+  }
+
+  @Test
+  public void fluxTestDelayNoOutput() {
+
+    Flux<Person> people = Flux.just(michael, fiona, sam, jesse);
+
+    people.delayElements(Duration.ofSeconds(1))
+        .subscribe(person -> log.info(person.sayMyName()));
+  }
+
+  @Test
+  public void fluxTestDelay() throws Exception {
+
+    CountDownLatch countDownLatch = new CountDownLatch(2);
+
+    Flux<Person> people = Flux.just(michael, fiona, sam, jesse);
+
+    people.delayElements(Duration.ofSeconds(1))
+        .doOnComplete(countDownLatch::countDown)
+        .subscribe(person -> log.info("first stream {}", person.sayMyName()));
+
+    people.delayElements(Duration.ofSeconds(1))
+        .doOnComplete(countDownLatch::countDown)
+        .subscribe(person -> log.info("second stream {}", person.sayMyName()));
+
+    countDownLatch.await();
+  }
+
+  @Test
+  public void fluxTestFilterDelay() throws Exception {
+
+    CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    Flux<Person> people = Flux.just(michael, fiona, sam, jesse);
+
+    people.delayElements(Duration.ofSeconds(1))
+        .filter(person -> person.getFirstName().contains("i"))
+        .doOnComplete(countDownLatch::countDown)
+        .subscribe(person -> log.info(person.sayMyName()));
+
+    countDownLatch.await();
+  }
+
+}
